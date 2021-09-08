0b5ad8af19c224a11ca914ffcfe666262ef77c85
solomonov <d.solomonov@infocom-s.ru>
2021-09-08 16:24:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPLections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Palladin> listPalladins = new List<Palladin>();
+            listPalladins.Add(new Palladin("123456Artos", 1000000000));
+            listPalladins.Add(new Palladin("Uther"));
+            ShowPalladinCollection(listPalladins);
+            for (int i = 0; i < 5; i++)
+            {
+                listPalladins[0].LevelUP();
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                listPalladins[1].LevelUP();
+            }
+            ShowPalladinCollection(listPalladins);
+            Console.ReadKey();
+        }
+        private static void ShowPalladinCollection(List<Palladin>  listPalladins)
+        {
+            foreach (Palladin item in listPalladins)
+            {
+                item.Show();
+            }
+        }
+    }
+}
